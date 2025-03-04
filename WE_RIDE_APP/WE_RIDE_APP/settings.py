d0412d1b4f17f7b2e71fed358fd6ae15d9c36869
Daniel <104313625+djtarulis@users.noreply.github.com>
2025-03-04 03:24:52 -0600

    Updated ALLOWED_HOSTS

--- WE_RIDE_APP/WE_RIDE_APP/settings.py
+++ WE_RIDE_APP/WE_RIDE_APP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key-for-local')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://weride.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['weride.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
